bd3441093b7f0ec4707ad409794ff885a0785d17
Diegol <diegoalmesp@gmail.com>
2023-11-02 17:56:21 -0300

    step completed

--- frontend/src/components/Input.svelte
+++ frontend/src/components/Input.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+  import debounce from 'lodash.debounce'
   export let search: string = '';
+
+  const onSearch = debounce((e: Event) => {
+    const term: string = (e.target as HTMLInputElement).value;
+    search = term
+  }, 800)
 </script>
 
 <input
-  bind:value={search}
   type="text" 
   class="block w-80 h-9 px-3 rounded"
-  placeholder="enter your search..." />
-
+  placeholder="enter your search..."
+  on:input={onSearch}
+/>
 
