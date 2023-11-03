53e13d3cdc7b80f49d49f260d623851520cae5ab
Diegol <diegoalmesp@gmail.com>
2023-11-03 09:32:51 -0300

    list github repo results

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,34 @@
 <script lang="ts">
   import Input from '$lib/components/Input.svelte'
-  import { getGithubRepoInfo } from '$lib/server'
+  import { getGithubRepoInfo } from '../utils/server'
 
   let search: string = ''
+
+  $: repos = getGithubRepoInfo(search)
 </script>
 
 <section class="w-full flex justify-center mb-6">
   <Input bind:search />
 </section>
 
-<h2 class="text-lg">Repos with the keyword: [{search}]</h2>
+<h2 class="text-lg mb-8">Repos with the keyword: [{search}]</h2>
+
+{#await repos}
+  <p>Loading...</p>
+{:then repos}
+  {#each repos as repo}
+    <div class="flex flex-col justify-center items-center mb-4">
+      <a
+        class="text-green-300 hover:underline font-mono"
+        href={repo.html_url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {repo.name}
+      </a>
+      <p class="text-sm text-gray-500">{repo.description}</p>
+    </div>
+  {/each}
+{:catch error}
+  <p>{error.message}</p>
+{/await}
--- frontend/src/utils/server/index.ts
+++ frontend/src/utils/server/index.ts
@@ -1,7 +1,9 @@
 export async function getGithubRepoInfo(name: string, language = 'typescript', results = 30) {
+	if (!name || name === '') return [];
+
 	// https://api.github.com/search/repositories?q=${name}+language:${language}&sort=${sort}&order=desc&per_page=${perPage}&page=${pageNum}
 	return fetch(
-		`https://api.github.com/search/repositories?q=${name}+language:${language}&sort=stars&order=desc&per_page=30&page=1`
+		`https://api.github.com/search/repositories?q=${name}+language:${language}&sort=stars&order=desc&per_page=${results}&page=1`
 	)
 		.then((res) => res.json())
 		.then((data) => {
